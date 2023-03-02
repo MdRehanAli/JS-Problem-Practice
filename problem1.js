36933aebb5aa94de9a840c2729f5f49293b25dd0
MdRehanAli <rehanali21d>
2023-03-02 19:38:12 +0600

    problem1.js added

---
+++ problem1.js
@@ -0,0 +1,13 @@
+function feetToInch(feet) {
+    if (typeof feet !== Number) {
+        console.log('Please Enter a number.');
+    }
+
+    const inches = feet *12;
+    return inches;
+
+}
+
+const feet = 5;
+const feets = feetToInch(feet);
+console.log(feet, 'feet =', feets, 'inches.');
